32a744770c1ec3de1f776ea62499f3c001fcc128
Divyam Tiwari <tiwarixdivyam@gmail.com>
2026-03-13 23:36:02 +0530

    feat: server setup

---
+++ Server/index.js
@@ -0,0 +1,10 @@
+import connectDB from "./src/config/db.js";
+import app from "./src/app.js";
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port : ${port}`));
+  })
+  .catch((error) => console.log(error));
